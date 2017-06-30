d9a010bdae22af0e623d7dc106e84677454ef14c
Clemens Sahs <clemens.sahs@laborato.de>
2017-06-30 18:08:47 +0200

    reproduction for react-apollo #793

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ import { NotFound, Redirect } from 'kit/lib/routing';
 
 // GraphQL queries
 import allMessages from 'src/queries/all_messages.gql';
+import message from 'src/queries/message.gql';
 
 // Styles
 import './styles.global.css';
@@ -43,61 +44,60 @@ import logo from './reactql-logo.svg';
 
 // ----------------------
 
-// We'll display this <Home> component when we're on the / route
-const Home = () => (
-  <h1>You&apos;re on the home page - click another link above</h1>
-);
-
-// Helper component that will be conditionally shown when the route matches.
-// This gives you an idea how React Router v4 works
-const Page = ({ match }) => (
-  <h1>Changed route: {match.params.name}</h1>
-);
-
-// Create a route that will be displayed when the code isn't found
-const WhenNotFound = () => (
-  <NotFound>
-    <h1>Unknown route - the 404 handler was triggered!</h1>
-  </NotFound>
-);
-
-// Specify PropTypes if the `match` object, which is injected to props by
-// the <Route> component
-Page.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.object,
-  }).isRequired,
-};
-
-// Stats pulled from the environment.  This demonstrates how data will
-// change depending where we're running the code (environment vars, etc)
-// and also how we can connect a 'vanilla' React component to an RxJS
-// observable source, and feed eventual values in as properties
-const Stats = () => {
-  const info = [
-    ['Environment', process.env.NODE_ENV],
-  ];
-
-  return (
-    <ul className={css.data}>
-      {info.map(([key, val]) => (
-        <li key={key}>{key}: <span>{val}</span></li>
-      ))}
-    </ul>
-  );
-};
 
 // Now, let's create a GraphQL-enabled component...
 
 // ... then, let's create the component and decorate it with the `graphql`
 // HOC that will automatically populate `this.props` with the query data
 // once the GraphQL API request has been completed
-@graphql(allMessages)
-class GraphQLMessage extends React.PureComponent {
+@graphql(allMessages, {
+  options: (props) => ({
+    ssr: true,
+    variables: {
+      first: 3,
+    }
+  })
+})
+class GraphQLMessageList extends React.PureComponent {
   static propTypes = {
     data: PropTypes.shape({
       allMessages: PropTypes.arrayOf(
         PropTypes.shape({
+          id: PropTypes.string.isRequired,
+        }),
+      ),
+    }),
+  }
+
+  render() {
+    const { data } = this.props;
+    if (data.loading) {
+      return <div>loading</div>;
+    }
+    return (
+      <div>
+        {data.allMessages.map(message => (
+          <GraphQLMessage key={message.id} id={message.id} />
+        ))}
+      </div>
+    );
+  }
+}
+
+@graphql(message, {
+  options: (props) => ({
+    ssr: false,
+    variables: {
+      id: props.id
+    },
+  })
+})
+class GraphQLMessage extends React.PureComponent {
+  static propTypes = {
+    data: PropTypes.shape({
+      Message: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string.isRequired,
           text: PropTypes.string.isRequired,
         }),
       ),
@@ -106,54 +106,21 @@ class GraphQLMessage extends React.PureComponent {
 
   render() {
     const { data } = this.props;
-    const message = data.allMessages && data.allMessages[0].text;
-    const isLoading = data.loading ? 'yes' : 'nope';
+    if (data.loading) {
+      return <div>loading</div>;
+    }
+
+    if (! data.Message) {
+      console.log(data, data.error);
+      return <div>error {this.props.id}</div>;
+    }
+
     return (
-      <div>
-        <h2>Message from GraphQL server: <em>{message}</em></h2>
-        <h2>Currently loading?: {isLoading}</h2>
-      </div>
+      <div>{data.Message.id}: {data.Message.text}</div>
     );
   }
 }
 
-// Example of CSS, SASS and LESS styles being used together
-const Styles = () => (
-  <ul className={css.styleExamples}>
-    <li className={css.example}>Styled by CSS</li>
-    <li className={sass.example}>Styled by SASS</li>
-    <li className={less.example}>Styled by LESS</li>
-  </ul>
-);
-
-// Sample component that demonstrates using a part of the Redux store
-// outside of Apollo.  We can import own custom reducers in `kit/lib/redux`
-// and 'listen' to them here
-@connect(state => ({ counter: state.counter }))
-class ReduxCounter extends React.PureComponent {
-  static propTypes = {
-    counter: PropTypes.shape({
-      count: PropTypes.number.isRequired,
-    }),
-  };
-
-  // Trigger the `INCREMENT_COUNTER` action in Redux, to add 1 to the total
-  triggerIncrement = () => {
-    this.props.dispatch({
-      type: 'INCREMENT_COUNTER',
-    });
-  }
-
-  render() {
-    const { count } = this.props.counter;
-    return (
-      <div>
-        <h2>Listening to Redux counter: {count}</h2>
-        <button onClick={this.triggerIncrement}>Increment</button>
-      </div>
-    );
-  }
-}
 
 // Export a simple component that allows clicking on list items to change
 // the route, along with a <Route> 'listener' that will conditionally display
@@ -166,32 +133,6 @@ export default () => (
         name: 'description',
         content: 'ReactQL starter kit app',
       }]} />
-    <div className={css.hello}>
-      <img src={logo} alt="ReactQL" className={css.logo} />
-    </div>
-    <hr />
-    <GraphQLMessage />
-    <hr />
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/page/about">About</Link></li>
-      <li><Link to="/page/contact">Contact</Link></li>
-      <li><Link to="/old/path">Redirect from /old/path &#8594; /new/path</Link></li>
-    </ul>
-    <hr />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/page/:name" component={Page} />
-      <Redirect from="/old/path" to="/new/path" />
-      <Route component={WhenNotFound} />
-    </Switch>
-    <hr />
-    <ReduxCounter />
-    <hr />
-    <p>Runtime info:</p>
-    <Stats />
-    <hr />
-    <p>Stylesheet examples:</p>
-    <Styles />
+    <GraphQLMessageList />
   </div>
 );
